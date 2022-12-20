67c90fc31fbb884b13a8ce51e40f7013e8f18474
susilthapa <thapasusil53@gmail.com>
2022-12-21 00:42:15 +0545

    Description height fixed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Card from "https://framer.com/m/Card-g0hh.js@GKNMhhPrE2uAQt2L89Wd";
+import Card from "https://framer.com/m/Card-g0hh.js@CdQu0gOsCaf4QUlk0fbm";
 
 import { Tip } from "../components/Tip";
 import pizzaImage from "../public/images/pizza.jpg";
